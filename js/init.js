57a133f8c182fc48712b81a0a95736c823280afe
iealon <959498764@qq.com>
2015-11-22 16:33:38 +0800

    add loading

--- js/init.js
+++ js/init.js
@@ -6,5 +6,10 @@ create  :
 加油少年！GO！GO！GO！
 \*----------------------------*/
 !function(){
-	
+	function getUrlParam(key,url){
+		var reg = new RegExp('.*[\?|\&|\#]'+key+'\=([^\&]*)\&?.*','ig');
+		url = url||location.href;
+		return url.replace(reg,'$1');
+	}
+
 }();
